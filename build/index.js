9df9b4c14c5379e6404f57c5b4fd74e9579e3e94
Sangopak,Kundu <sangojumech07@gmail.com>
2024-02-17 09:00:59 -0500

    Deleting the build folder

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var hello_world_1 = require("./services/hello-world");
-var message = new hello_world_1.HelloWorld("Hello World").getMessage("Snago");
-console.log("Message is ".concat(message));
