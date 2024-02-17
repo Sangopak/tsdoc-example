4c858107d5a7ef1e405c1dab3f3fbcf33eae1cdb
Sangopak,Kundu <sangojumech07@gmail.com>
2024-02-17 09:59:24 -0500

    Adding some JSDoc and updated script to expand the src dir

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { MessageService } from "./services/message";
 
-function main() {
-    const message = new MessageService("Hello World").getMessage("Sango");
+/**
+ * Main function to call the message service
+ * 
+ * @param greetings - first input string
+ * @param name - second input string
+ * @returns {void}
+ */
+function main(greetings: string, name: string): void {
+    const message = new MessageService(greetings).getMessage(name);
     console.log(message)
 }
 
-main()
+main("Hello!!", "Sango")
--- src/services/message.ts
+++ src/services/message.ts
@@ -1,10 +1,21 @@
 export class MessageService {
     private message: string;
 
+    /**
+     * Constructor
+     * 
+     * @param value - input string for greetings
+     */
     constructor(private value:string){
         this.message = value;
     }
 
+    /**
+     * Simple message function that concates message and name
+     * 
+     * @param name - first input string
+     * @returns {string}
+     */
     public getMessage(name: string) : string {
         return `${this.message} ${name}`
     }
